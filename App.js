9ce10da1d777e5c4e8d615be6b42c131f04f7668
Robert <127435909+robifal@users.noreply.github.com>
2023-08-23 16:29:54 -0300

    Terminado.

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
 
-export default function App() {
+const App1 = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Username:</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
+        placeholder='User'
+      />
+      <Text>Password:</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+        placeholder='Senha'
+      />
+      <Button title="Entrar" onPress={handleLogin}
+       style={styles.Button}
+      />
+      
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B0C4DE',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: '80%',
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+  },
 });
+
+export default App1;
